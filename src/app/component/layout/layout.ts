312ee290d3360561497bced768608094e3e6e768
Amaury Hernandez <Amauhuero@gmail.com>
2026-07-08 01:17:37 -0600

    Implementa registro de usuarios con Angular, PHP y MySQL

--- src/app/component/layout/layout.ts
+++ src/app/component/layout/layout.ts
@@ -1,28 +1,77 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
 export class Layout {
 
-
   isLoginOpen = false;
+  modo: 'login' | 'registro' = 'login';
 
-  openLogin(): void {
-    console.log('ABRIENDO MODAL');
+  mensaje = '';
+
+  registro = {
+    nombre: '',
+    ap_pat: '',
+    ap_mat: '',
+    correo: '',
+    telefono: '',
+    contrasena: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  openLogin() {
     this.isLoginOpen = true;
+    this.modo = 'login';
+    this.mensaje = '';
   }
 
-
-  closeLogin(): void {
-    console.log('CERRANDO MODAL');
+  closeLogin() {
     this.isLoginOpen = false;
+    this.mensaje = '';
   }
 
+  cambiarRegistro() {
+    this.modo = 'registro';
+    this.mensaje = '';
+  }
+
+  cambiarLogin() {
+    this.modo = 'login';
+    this.mensaje = '';
+  }
+
+  registrarUsuario() {
+    this.http.post<any>('http://localhost/NovusAPI/registro.php', this.registro)
+      .subscribe({
+        next: (res) => {
+          this.mensaje = res.mensaje;
+
+          if (res.success) {
+            this.registro = {
+              nombre: '',
+              ap_pat: '',
+              ap_mat: '',
+              correo: '',
+              telefono: '',
+              contrasena: ''
+            };
+
+            this.modo = 'login';
+          }
+        },
+        error: () => {
+          this.mensaje = 'Error al conectar con el servidor.';
+        }
+      });
+  }
 }
