bb5197d761aff6974d7ba2c81e094e2963529e09
Miguel Angel Moran Casimiro <miguelangelmorancasimirod@gmail.com>
2026-07-10 03:43:52 -0600

    Feat: Se añade el diseño responsivo de la pantalla de inicio Novus

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  // Al llamarse 'Home' coincidirá con lo que tus rutas ya están buscando automáticamente
 }
