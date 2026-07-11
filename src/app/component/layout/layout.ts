dac1faa4a919c7de0ea231773382fc9faa10475d
Amaury Hernandez <Amauhuero@gmail.com>
2026-07-10 19:43:13 -0600

    Feat: reconstrucción del módulo de autenticación

--- src/app/component/layout/layout.ts
+++ src/app/component/layout/layout.ts
@@ -1,77 +1,199 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
+
+  /*
+    CommonModule permite usar directivas como *ngIf.
+    RouterLink permite navegar con routerLink.
+    RouterOutlet muestra el componente de la ruta activa.
+  */
+  imports: [CommonModule, RouterLink, RouterOutlet,FormsModule],
+
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
+
 export class Layout {
-
+  /*Modal que controla si abre o cierra */
   isLoginOpen = false;
-  modo: 'login' | 'registro' = 'login';
+ 
+  /*Como el formulario se mostrara */
+  modo:'login'|'registro'='login';
+ 
+  /*Se crea un objeto */
+  login = {
 
-  mensaje = '';
-
-  registro = {
-    nombre: '',
-    ap_pat: '',
-    ap_mat: '',
     correo: '',
-    telefono: '',
+
     contrasena: ''
+
   };
+   
+  mensaje = '';
+registro ={
+  nombre:'',
+  ap_pat:'',
+  ap_mat:'',
+  correo:'',
+  telefono:'',
+  contrasena:''
+};
 
-  constructor(private http: HttpClient) {}
+  /*Constructor  */
+  constructor(private http:HttpClient){}
 
+ 
+
+  /*Abre el login */
   openLogin() {
     this.isLoginOpen = true;
     this.modo = 'login';
-    this.mensaje = '';
+    this.mensaje = ''; 
   }
 
+
+
+  /*Cierra el login */
   closeLogin() {
     this.isLoginOpen = false;
-    this.mensaje = '';
-  }
 
-  cambiarRegistro() {
+  }
+  cambiarRegistro(){
     this.modo = 'registro';
     this.mensaje = '';
   }
-
-  cambiarLogin() {
+  cambiarLogin(){
     this.modo = 'login';
-    this.mensaje = '';
+    this.mensaje='';
   }
 
-  registrarUsuario() {
-    this.http.post<any>('http://localhost/NovusAPI/registro.php', this.registro)
-      .subscribe({
-        next: (res) => {
-          this.mensaje = res.mensaje;
 
-          if (res.success) {
-            this.registro = {
-              nombre: '',
-              ap_pat: '',
-              ap_mat: '',
-              correo: '',
-              telefono: '',
-              contrasena: ''
-            };
+iniciarSesion(){
 
-            this.modo = 'login';
-          }
-        },
-        error: () => {
-          this.mensaje = 'Error al conectar con el servidor.';
+  if (
+/*Comprobacion si esta vacia la casilla */
+this.login.correo === '' ||
+/*Comrpobacion si la casilla esta vacia con comprobacion */
+this.login.contrasena === ''
+
+  ){
+    this.mensaje = 'Todos los campos son obligatorios'
+   return;
+  }
+  /*Usa la herramienta httpclient */
+  this.http
+  /*Peticion de tipo post y la direccion donde los recibe */  
+  .post<any>('http//localhost/NovusApi/login.php',
+    /*Objeto  */  
+    this.login
+    )
+    /*Ejecuta la peticion y  obtiene respuesta */
+   .subscribe({
+    /*Responde cuando php es true */
+      next: (res) => {
+        /*Guarda el mensaje */
+        this.mensaje = res.mensaje;
+        /*Si el login fue correcto */
+        if (res.success) {
+          /*Guarda datos del usuario en el navegador  */
+          localStorage.setItem(
+            'usuario',
+            /*Convierte el objeto en texto */
+            JSON.stringify(res.usuario)
+          );
+
+          this.login = {
+            correo: '',
+            contrasena: ''
+          };
+
+          this.closeLogin();
         }
-      });
-  }
+      },
+
+      error: () => {
+
+        this.mensaje = 'Error al conectar con el servidor';
+
+      }
+
+    });
 }
+
+registrarUsuario(){
+  /*Verifica que ningun campo este vacio */
+  if(
+    this.registro.nombre === ''||
+    this.registro.ap_pat === ''||
+    this.registro.ap_mat === ''||
+    this.registro.telefono === ''||
+    this.registro.correo === ''||
+    this.registro.contrasena === ''
+
+  ){
+    this.mensaje = 'Todos los campos son obligatorios';
+    return;
+
+  }
+  /*Envie el registro */
+  this.http
+  .post<any>(
+    'http://localhost/NovusApi/registro.php',
+    this.registro
+  )
+.subscribe({
+  /*Llega a php y responde */
+  next:(res) =>{
+    this.mensaje = res.mensaje;
+    /*Si el registro fue correcto */
+    if(res.success){
+      this.registro= {
+        nombre:'',
+        ap_pat:'',
+        ap_mat:'',
+        telefono:'',
+        correo:'',
+        contrasena:''
+      };
+      this.modo ='login';
+    }
+  },
+  /*Si no fue posible */
+  error:()=>{
+  this.mensaje = 'Error al registrar';
+  }
+});
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
